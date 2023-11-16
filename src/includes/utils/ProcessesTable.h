e052fa92a4f1cddf30f6bcf231c7d4b477e0c576
Saber Mahjoub <saber.mahjoub003@gmail.com>
2023-11-16 13:21:51 +0100

    Final Version of the parsing of the processes file

--- src/includes/utils/ProcessesTable.h
+++ src/includes/utils/ProcessesTable.h
@@ -13,146 +13,76 @@ struct Process {
     int priority;
 };
 
-//A Function that extracts the maximum number of processes from the configuration file.
-int Max_nb_processes() {
+//The function that tokenizes a string and return a Process struct.
+void tokenize (char ch[255],char del[],struct Process *P) {
+    ch[strcspn(ch, "\n")] = '\0';
 
-    FILE* fPointer = fopen ("../../config.conf","r");
-    char content[100] ;
+    char *token;
 
-    //const char *delimiter = "=";
-    //char *token;
-    int value;
+    //an integer to determine which Process Attribute we are parsing;
+    int nb=1;
 
-    if (fPointer != NULL ) {
-            printf("Lecture of conf file is successfull ! \n");
-            fgets(content,100,fPointer);
+    //struct Process P;
 
-        // Find the position of "=" in the string
-        if (sscanf(content, "%*[^=]=%d", &value) == 1) {
-            return value;
+    // Tokenize the string using strtok
+    token = strtok(ch, del);
+
+    while (token != NULL) {
+        if (nb==1){
+            strcpy(P->name,token);
         }
-        else {
-        return -1;
+        if (nb==2){
+            P->arrivalTime=atoi(token);
         }
-
+        if (nb==3){
+            P->ExecTime=atoi(token);
+        }
+        if (nb==4){
+            P->priority=atoi(token);
+            break;
+        }
+        nb++;
+        token = strtok(NULL, del);
     }
-    
-    else {
-        printf("ERROR opening the conf file ! \n");
-        return -1;
-
-    }
-    fclose(fPointer);
 
 }
 
 
-//The function that tokenize a string and return a Process struct.
-struct Process tokenize (char ch[255],char del[]) {
-    ch[strcspn(ch, "\n")] = '\0';
+struct Process *getTableOfProcesses() {
+    //Choosing the seperator character 
+    char sep[] = ";";
 
-    //Variables of process details
-    char ProcessName[10]="";
-    char arrivalTime[10]="";
-    char ExecTime[10]="";
-    char priority[10] ="";
-    int ind=0; int indAT=0; int indEX=0; int indPR=0;
+    //The size of the processes table
+    int sizeTable;
+    int i=0;
 
-    for (int i=0;i<strlen(ch);i++) {
-        if (ind==0){
-            if (ch[i]==':'){
-                ind++;
-                continue;
-            }
-            else {
-                ProcessName[i]=ch[i];
-                }
-        }
-        else if (ind==1) {
-            if (ch[i]==del[0]){
-                ind++;
-                continue;
-            }
-            else {
-            arrivalTime[indAT]=ch[i];
-            indAT++;
-            }
-        }
-        else if (ind==2) {
-            if (ch[i]==del[0]){
-                ind++;
-                continue;
-            }
-            else {
-            ExecTime[indEX]=ch[i];
-            indEX++;
-            }
-        }
-        else if (ind==3) {
-            priority[indPR]=ch[i];
-            indPR++;
-            }
-        }
-
-        struct Process P;
-        strcpy(P.name, ProcessName);
-        P.arrivalTime = atoi(arrivalTime);
-        P.ExecTime = atoi(ExecTime);
-        P.priority = atoi(priority);
-
-        return P;
-    }
-
-
-int main(){
-    // Choosing the seperator character.
-    char sep[]=";";
-
-    //Getting the size of the table of processes
-    int sizeTable=Max_nb_processes();
-
-    FILE* fPointer = fopen ("test.txt","r");
-    char content[255] ;
-
-    //Number of lines
-    int line=1;
-    //Position of the table
-    int i;
-
-    struct Process processes[sizeTable];
+    FILE *fPointer= fopen("test.txt","r");
+    char content[255];
 
     if (fPointer != NULL ) {
-            printf("Lecture is successfull ! \n");
-            while(!feof(fPointer)){
-                if (line==1){
-                    fgets(content,255,fPointer);
-                    sizeTable = atoi(content);
-                    struct Process processes[sizeTable];
-                    printf("Size of the table is : %d \n",sizeTable);
-                    i=0;
-                    line++;
-                    // We skip the second line \n ????
-                    fgets(content,255,fPointer);
-                    continue;
-                    }
+            printf("Lecture of the processes file is successfull ! \n");
+            //Getting the number of processes
+            fgets(content,255,fPointer);
+            sizeTable = atoi(content);
+            struct Process* processes = (struct Process*)malloc(sizeTable * sizeof(struct Process));
+            //printf("Size of the table is : %d \n",sizeTable);
 
+            while(!feof(fPointer) && i < sizeTable){
                 fgets(content,255,fPointer);
-                struct Process p = tokenize(content,sep);
-                processes[i]=p;
+                //struct Process p = tokenize(content,sep);
+                tokenize(content, sep, &processes[i]);
+                //processes[i]=p;
                 i++;
-                line++;
             }
-            //Testinng part
-            printf("%d",processes[0].priority);
+            fclose(fPointer);
 
+            //Testinng part
+            //printf("%d",processes[0].priority);
+            return processes;
     }
     else {
         printf("Error opening the processes file");
-        return -1;
+        return NULL;
     }
-
-    fclose(fPointer);
-
-    return 0;
 }
 
