cc52df19013fe0d6b9aed9777527a6e5071221ab
Maha-Houidi <maha.houidi@etudiant-isi.utm.tn>
2023-11-17 23:01:01 +0100

    MSASS-37: add static preemptive priority scheduling algorithm

--- src/scheduling_algorithms/static_priority.c
+++ src/scheduling_algorithms/static_priority.c
@@ -36,6 +36,71 @@ void static_priority_scheduler(Process processes[], int n) {
 }
 //TODO: calculate the metrics for static priority : average turnaround time (rotation time) // average waiting time 
 
+/******************************************************************
+    PURPOSE: Preemptive Priority Scheduler
+    PARAMETERS:
+    - n : number of processes to be scheduled in the system
+    - processes[] : a sorted array of the processes to be scheduled
+******************************************************************/
+void preemptive_priority_scheduler(Process processes[], int n) {
+    PriorityQueue *pq = init_priority_queue(n, sizeof(Process), compare_process_priority);
+
+    int currentTime = 0;
+    int i = 0;
+    Process* currentProcess = NULL;
+
+    while (i < n) {
+        while (i < n && processes[i].arrivalTime <= currentTime) {
+            push(pq, &processes[i]);
+            i++;
+        }
+
+        if (currentProcess != NULL) {
+            push(pq, currentProcess);
+        } 
+
+        if (!is_empty(pq)) {
+            Process *highestPriorityProcess = pop(pq);
+
+            if (currentProcess != NULL && highestPriorityProcess->priority > currentProcess->priority) {
+                printf("Preempting Process %s at time %d\n", currentProcess->processName, currentTime);
+                // Re-insert current process into the pq 
+                //currentProcess->runTime --;
+                //push(pq, currentProcess); 
+                // clear current process to empty the CPU
+                //free(currentProcess);
+                currentProcess = NULL; 
+            }
+
+            if (currentProcess == NULL) {
+                currentProcess = (Process*)malloc(sizeof(Process));
+                //currentProcess = highestPriorityProcess;
+                memcpy(currentProcess, highestPriorityProcess, sizeof(Process));
+                printf("Executing Process %s at time %d\n", currentProcess->processName, currentTime);
+            }
+
+            currentProcess->runTime--;
+            currentTime++;
+
+            if (currentProcess->runTime == 0) {
+                printf("Process %s has terminated his job and left the CPU at time %d\n", currentProcess->processName, currentTime);
+                //i++;
+                // clear current process to empty the CPU
+                free(currentProcess);
+                currentProcess = NULL;
+            }
+        } else {
+            currentTime++;
+        }
+    } 
+
+}
+
+//TODO: fix the preemptive algorithm problem
+//TODO: calculate the metrics for pre-emptive priority : average turnaround time (rotation time) // average waiting time 
+
+
+
 
 // Example usage
 int main() {
