e3942a84f8e76436a05db658c7ef7aa818720e0a
amine-a11 <mohamedaminehajji11@yahoo.com>
2023-11-21 22:08:22 +0100

    update

--- src/scheduling_algorithms/multilevel.c
+++ src/scheduling_algorithms/multilevel.c
@@ -4,69 +4,85 @@
 #include "../includes/data_structures/queue.h"
 
 #include "../includes/utils/ProcessesTable.h"
-
+#include<time.h>
 Queue* processesQ;
 
+typedef struct MultitaskProcess{
+    Process process;
+    int readyFrom;
+    int new;
+}MultitaskProcess;
 
-// int compare_process_priority(const void *a, const void *b) {
-//     return ((Process*)a)->priority > ((Process*)b)->priority;
-// }
-int compare_process_priority_arrivalTime(const void *a, const void *b) {
-    return ((Process*)a)->arrivalTime < ((Process*)b)->arrivalTime;
-}
+
+
 int compare_process_priority_multilevel(const void *a, const void *b) {
-    if(((Process*)a)->priority < ((Process*)b)->priority){
+    if((((MultitaskProcess*)a)->process).priority < (((MultitaskProcess*)b)->process).priority){
         return -1;
-    }else if(((Process*)a)->priority > ((Process*)b)->priority){
+    }else if((((MultitaskProcess*)a)->process).priority > (((MultitaskProcess*)b)->process).priority){
         return 1;
     }else{
-        if(((Process*)a)->arrivalTime > ((Process*)b)->arrivalTime){
+        if(((MultitaskProcess*)a)->readyFrom > ((MultitaskProcess*)b)->readyFrom){
             return -1;
-        }else if(((Process*)a)->arrivalTime < ((Process*)b)->arrivalTime){
+        }else if(((MultitaskProcess*)a)->readyFrom < ((MultitaskProcess*)b)->readyFrom){
             return 1;
+        }else{
+            if(((MultitaskProcess*)a)->new > ((MultitaskProcess*)b)->new){
+                return 1;
+            }else{
+                return -1;
+            }
         }
-        return 1;
     }
 }
 
 void multilevel(int quantum,int size){
     int t=0;
-    PriorityQueue* pq = init_priority_queue(size,sizeof(Process),compare_process_priority_multilevel);
+    PriorityQueue* pq = init_priority_queue(size,sizeof(MultitaskProcess),compare_process_priority_multilevel);
     while(processesQ->front != NULL || pq->size!=0){
         if(processesQ->front != NULL){
             while(processesQ->front != NULL && processesQ->front->data.arrivalTime==t){
-                Process pp=dequeue(processesQ);
+                Process process=dequeue(processesQ);
+                MultitaskProcess pp= {0};
+                pp.process=process;
+                pp.readyFrom=t;
+                pp.new=0;
                 push(pq,&pp);
             }
         }
         if(pq->size!=0){
-            Process* p = (Process *)pop(pq);
+            MultitaskProcess* p = (MultitaskProcess *)pop(pq);
 
             for(int i=0;i<quantum;i++){
-                p->runTime--;
+                (p->process).runTime--;
                 t++;
-                printf("%s is running form t = %d to t = %d \n",p->processName,t-1,t);
+                printf("%s is running form t = %d to t = %d \n",(p->process).processName,t-1,t);
                 int isPriorityBigger=0;
                 if(processesQ->front != NULL){
                     while(processesQ->front != NULL && processesQ->front->data.arrivalTime==t){
-                        Process pp = dequeue(processesQ);
-                        if(pp.priority>p->priority){
+                        Process process=dequeue(processesQ);
+                        MultitaskProcess pp= {0};
+                        pp.process=process;
+                        pp.readyFrom=t;
+                        pp.new=process.priority==(p->process).priority?1:0;
+                        if(process.priority>(p->process).priority){
                             isPriorityBigger=1;
                         }
                         push(pq,&pp);
                     }
                 }
                 if(isPriorityBigger){
-                    if(p->runTime!=0){
-                        p->arrivalTime=t;
+                    if((p->process).runTime!=0){
+                        p->readyFrom=t;
+                        p->new=0;
                         push(pq,p);
                     }
                     break;
                 }else{
-                    if(p->runTime==0){
+                    if((p->process).runTime==0){
                         break;
                     }else if(i==quantum-1){
-                        p->arrivalTime=t;
+                        p->readyFrom=t;
+                        p->new=0;
                         push(pq,p);
                     }
                 }
@@ -85,24 +101,31 @@ int main(void){
     // struct Process *proc = getTableOfProcesses();
 
     // generate_processes_file("./src/config.conf", "./src/processes.txt", ';');
-    int processes_number = getNbProcesses("./src/processes.txt");
-    Process* processes = getTableOfProcesses("./src/processes.txt");
+    // int processes_number = getNbProcesses("./src/processes.txt");
+    // Process* processes = getTableOfProcesses("./src/processes.txt");
 
-    // Process processes[8] = {
-    //     {"P1", 0, 6, 2},
-    //     {"P2", 1, 4, 3},
-    //     {"P3", 2, 5, 3},
-    //     {"P4", 4, 2, 1},
-    //     {"P5", 5, 4, 1},
-    //     {"P6", 6, 5, 2},
-    //     {"P7", 12, 5, 4},
-    //     {"P8", 13, 2, 4},
-    // };
+    Process processes[8] = {
+        {"P1", 0, 6, 2},
+        {"P2", 1, 4, 3},
+        {"P3", 2, 5, 3},
+        {"P4", 4, 2, 1},
+        {"P5", 5, 4, 1},
+        {"P6", 6, 5, 2},
+        {"P7", 12, 5, 4},
+        {"P8", 13, 2, 4},
+    };
 
-    processesQ = create_queue_from_array(processes,processes_number);
-    
+    processesQ = create_queue_from_array(processes,8);
+    clock_t start,end;
+    double cpu_time_used;
 
-    multilevel(2,processes_number);
+    start=clock();
+    multilevel(2,8);
+    end=clock();
+    cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+
+    printf("time = %f ",cpu_time_used);
+
     // for(int i=0;i<8;i++){
     //     push(processesQ,&processes[i]);
     // }
