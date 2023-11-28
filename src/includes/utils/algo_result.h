3f5271c1b53731576304cb4ee44d17c19bc5784b
amine-a11 <mohamedaminehajji11@yahoo.com>
2023-11-28 19:36:19 +0100

    implemented all functions

---
+++ src/includes/utils/algo_result.h
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct InstantResultNode{
+    int t;
+    char* processName; // null <=> idle state
+    char** readyQueue;
+    int readyQueueSize;
+    int quit; // 1 = finished
+    struct InstantResultNode* next;
+}InstantResultNode;
+typedef struct Metrics{
+    float averageRotation;
+    float averageWaiting;
+}Metrics;
+typedef struct Gantt{
+    InstantResultNode* front;
+    InstantResultNode* rear;
+}Gantt;
+typedef struct AlgoResult{
+    Gantt* gantt;
+    Metrics metrics;
+}AlgoResult;
+// Methods
+Gantt* create_gantt();
+int is_empty_gantt(Gantt* ganttQueue);
+void enqueue_gantt(Gantt* ganttQueue, InstantResultNode instantResultNode);
+InstantResultNode dequeue_gantt(Gantt* ganttQueue);
+InstantResultNode* create_instant_result_node(InstantResultNode instantResultNode);
+void add_metrics(AlgoResult* algoResult,float averageRotation,float averageWaiting);
+
+
+Gantt* create_gantt(){
+    Gantt* gantt = (Gantt*)malloc(sizeof(Gantt));
+    if(!gantt){
+        printf("[ERROR] : Memory Allocation has failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    gantt->front = NULL;
+    gantt->rear  = NULL;
+}
+
+int is_empty_gantt(Gantt* ganttQueue){
+    return (ganttQueue->front==NULL);
+}
+
+InstantResultNode* create_instant_result_node(InstantResultNode instantResultNode){
+    InstantResultNode* newInstantResultNode = (InstantResultNode*)malloc(sizeof(InstantResultNode));
+    if(!newInstantResultNode){
+        printf("[ERROR] : Memory Allocation has failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    newInstantResultNode->t              = instantResultNode.t;
+    newInstantResultNode->processName    = instantResultNode.processName;
+    newInstantResultNode->quit           = instantResultNode.quit;
+    newInstantResultNode->readyQueue     = instantResultNode.readyQueue;
+    newInstantResultNode->readyQueueSize = instantResultNode.readyQueueSize;
+    newInstantResultNode->next           = NULL;
+    return newInstantResultNode;
+
+}
+
+
+void enqueue_gantt(Gantt* ganttQueue, InstantResultNode instantResultNode){
+    InstantResultNode* newInstantResultNode = create_instant_result_node(instantResultNode);
+    
+    if(is_empty_gantt(ganttQueue)){
+        ganttQueue->front = newInstantResultNode;
+        ganttQueue->rear  = newInstantResultNode;
+    }else{
+        ganttQueue->rear->next = newInstantResultNode;
+        ganttQueue->rear       = newInstantResultNode;
+    }
+}
+
+InstantResultNode dequeue_gantt(Gantt* ganttQueue){
+    if(is_empty_gantt(ganttQueue)){
+        printf("[ERROR] : Gantt queue is already empty\n");
+        exit(EXIT_FAILURE);
+    }
+    InstantResultNode* front            = ganttQueue->front;
+    InstantResultNode instantResultNode = *front;
+    ganttQueue->front                   = front->next;
+    free(front);
+    return instantResultNode;
+}
+
+void add_metrics(AlgoResult* algoResult,float averageRotation,float averageWaiting){
+    algoResult->metrics.averageRotation = averageRotation;
+    algoResult->metrics.averageWaiting  = averageWaiting;
+}
+
+
+
+// example :
+// #include<stdio.h>
+// #include "algo_result.h"
+
+// int main(void){
+//     AlgoResult result;
+//     result.gantt=create_gantt();
+//     add_metrics(&result,1.4,1.5);
+//     char** list = (char**)malloc(3*sizeof(char*));
+//     list[0]="hi";
+//     list[1]="hello";
+//     list[2]="by";
+//     for(int i=0;i<10;i++){
+//         InstantResultNode node={
+//             i,
+//             "hello",
+//             list,
+//             3,
+//             0,
+//             NULL
+//         };
+//         enqueue_gantt(result.gantt,node);
+//     }
+//     while(!is_empty_gantt(result.gantt)){
+//         InstantResultNode node = dequeue_gantt(result.gantt);
+//         printf("%d\n",node.t);
+//     }
+//     printf("%f %f\n",result.metrics.averageRotation,result.metrics.averageWaiting);
+//     return 1;
+// }
