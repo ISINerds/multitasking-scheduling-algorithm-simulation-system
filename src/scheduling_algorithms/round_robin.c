455a9051884294599991ba1b0a8952397e6689ee
mehrez bey <mahrezbey@gmail.com>
2023-12-01 20:57:01 +0100

    updating the function prototype

--- src/scheduling_algorithms/round_robin.c
+++ src/scheduling_algorithms/round_robin.c
@@ -4,8 +4,7 @@
 #include "../includes/utils/ProcessesTable.h"
 #include "../includes/utils/algo_result.h"
 
-// ------------------ Round Robin  Algorithm
-AlgoResult Round_Robin(Queue* queue,int processes_number,int quantum){
+AlgoResult round_robin(Queue* queue,int processes_number,int quantum){
 
     if(is_empty_q(queue)){
         exit(EXIT_FAILURE);
@@ -68,32 +67,32 @@ AlgoResult Round_Robin(Queue* queue,int processes_number,int quantum){
         return algoResult;
 }
 
-int main(void){
-    Process processes[10] = {
-        {"Process1", 0, 5, 3},
-        {"Process2", 1, 3, 2},
-        {"Process3", 2, 1, 4},
-        {"Process4", 4, 2, 1},
+// int main(void){
+//     Process processes[10] = {
+//         {"Process1", 0, 5, 3},
+//         {"Process2", 1, 3, 2},
+//         {"Process3", 2, 1, 4},
+//         {"Process4", 4, 2, 1},
         
-    };
-    int processes_number = 4;
-    Queue* q = create_queue_from_array(processes,processes_number);
-    int quantum = 2;
-    AlgoResult result =  Round_Robin(q,processes_number,quantum);
-    while (!is_empty_gantt(result.gantt)){
-        InstantResultNode ins =  dequeue_gantt(result.gantt);
-        printf("t = %d \n",ins.t);
-        printf("process name = %s \n",ins.processName);
-        printf("finished = %d \n",ins.quit);
-        printf("ready queue size = %d \n",ins.readyQueueSize);
-        for(int i=0;i<ins.readyQueueSize;i++){
-            printf(ins.readyQueue[i]);
-            printf("|");
-        }
-        printf("\n");
-        printf("--------------------\n");
-    }
-    printf(" Average Waiting Time %.2f\n",result.metrics.averageWaiting);
-    printf(" Average Rotaion Time %.2f\n",result.metrics.averageRotation);
-    return 0;
-}
+//     };
+//     int processes_number = 4;
+//     Queue* q = create_queue_from_array(processes,processes_number);
+//     int quantum = 2;
+//     AlgoResult result =  round_robin(q,processes_number,quantum);
+//     while (!is_empty_gantt(result.gantt)){
+//         InstantResultNode ins =  dequeue_gantt(result.gantt);
+//         printf("t = %d \n",ins.t);
+//         printf("process name = %s \n",ins.processName);
+//         printf("finished = %d \n",ins.quit);
+//         printf("ready queue size = %d \n",ins.readyQueueSize);
+//         for(int i=0;i<ins.readyQueueSize;i++){
+//             printf(ins.readyQueue[i]);
+//             printf("|");
+//         }
+//         printf("\n");
+//         printf("--------------------\n");
+//     }
+//     printf(" Average Waiting Time %.2f\n",result.metrics.averageWaiting);
+//     printf(" Average Rotaion Time %.2f\n",result.metrics.averageRotation);
+//     return 0;
+// }
