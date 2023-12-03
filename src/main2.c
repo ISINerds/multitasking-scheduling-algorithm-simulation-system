bbfadce7828a995f0e2d038bfa43d23dd2751788
amine-a11 <mohamedaminehajji11@yahoo.com>
2023-12-03 21:23:57 +0100

    add layout

--- src/main2.c
+++ src/main2.c
@@ -1,82 +1,96 @@
-/*******************************************************************************************
-*
-*   raygui - custom sliders
-*
-*   DEPENDENCIES:
-*       raylib 4.0  - Windowing/input management and drawing.
-*       raygui 3.0  - Immediate-mode GUI controls.
-*
-*   COMPILATION (Windows - MinGW):
-*       gcc -o $(NAME_PART).exe $(FILE_NAME) -I../../src -lraylib -lopengl32 -lgdi32 -std=c99
-*
-*   LICENSE: zlib/libpng
-*
-*   Copyright (c) 2016-2023 Ramon Santamaria (@raysan5)
-*
-**********************************************************************************************/
-
-#include "raylib.h"
+#include "./includes/utils/dynamic_menu.h"
+#include "./includes/utils/processes_generator.h"
+#include "./includes/utils/ProcessesTable.h"
+#include "./includes/utils/algo_result.h"
 
 #define RAYGUI_IMPLEMENTATION
 #include "../build_raylib/_deps/raygui-src/src/raygui.h"
 #include "raylib.h"
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+
+#define textPadding 10
+#define borderRadius 0.1
+#define gap 5
+#define padding 5
+#define textSize 30
+#define containerColor (Color){ 220, 220, 220, 255 }
+#define backgroundColor (Color){255,255,255,1}
+Font font;
+
+Algorithm* algorithms = NULL;
+Process* processes = NULL;
+AlgoResult algoResult;
+
+int started=0;
+
+void render_job_pool(Rectangle boundry){
+    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
+    DrawTextEx(font,"Job pool",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+}
+void render_gantt(Rectangle boundry){
+    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
+    DrawTextEx(font,"Gantt",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+}
+void render_stats(Rectangle boundry){
+    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
+    DrawTextEx(font,"Stats",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+}
+void render_menu(Rectangle boundry){
+    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
+    DrawTextEx(font,"Menu",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+}
+void show_gantt_stats(Rectangle ganttBoundry,Rectangle statsBoundry){
+    render_gantt(ganttBoundry);
+    render_stats(statsBoundry);
+    // sleep(1);
+}
+void preview_screen(void){
+    int w = GetRenderWidth();
+    int h = GetRenderHeight();
+    BeginDrawing(); 
+    ClearBackground(backgroundColor);
+    Rectangle jobPoolRect = {
+        .x = 0+padding,
+        .y = 0+padding,
+        .width = (w-2*padding-2*gap)/3,
+        .height = 2*(h-gap)/3,
+    };
+    render_job_pool(jobPoolRect);
+    Rectangle menuRect = {
+        .x = 2*gap+padding+2*(w-2*padding-2*gap)/3,
+        .y = 0+padding,
+        .width = (w-2*padding-2*gap)/3,
+        .height = 2*(h-gap)/3,
+    };
+    render_menu(menuRect);
+    Rectangle statsRect = {
+        .x = gap+padding+(w-2*padding-2*gap)/3,
+        .y = 0+padding,
+        .width = (w-2*padding-2*gap)/3,
+        .height = 2*(h-gap)/3,
+    };
+    Rectangle ganttRect = {
+        .x = padding,
+        .y = 2*h/3 + gap,
+        .width = w-2*padding,
+        .height = (h-gap)/3 - padding,
+    };
+    show_gantt_stats(ganttRect,statsRect);
+    EndDrawing();
+
+}
+int main(void){
+    const int screenWidth = 900;
+    const int screenHeight = 500;
  
     InitWindow(screenWidth, screenHeight, "Multitasking Scheduling Algorithm Simulation System");
- 
-    // TODO: Load resources / Initialize variables at this point
+    font = LoadFont("../assets/fonts/DelaGothicOne-Regular.ttf");
  
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
- 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update variables / Implement example logic at this point
-        //----------------------------------------------------------------------------------
- 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
- 
-            ClearBackground(RAYWHITE);
- 
-            // TODO: Draw everything that requires to be drawn at this point:
- 
- 
-            DrawRectangle(5, 5, 392.5, 175, (Color){ 220, 220, 220, 255 });
-            DrawRectangle(402.5, 5, 392.5, 175, (Color){ 220, 220, 220, 255 });
-            DrawRectangle(5, 185, 260, 130, (Color){ 220, 220, 220, 255 });
-            DrawRectangle(270, 185, 260, 130, (Color){ 220, 220, 220, 255 });
-            DrawRectangle(535, 185, 260, 130, (Color){ 220, 220, 220, 255 });
-            DrawRectangle(5, 320, 790, 125, (Color){ 220, 220, 220, 255 });
-            DrawText("Data", 20, 15, 25, RED);
-            DrawText("Jobs", 417.5, 15, 25, RED);
-            DrawText("Cpu", 20, 195, 25, RED);
-            DrawText("Ready Queue", 285, 195, 25, RED);
-            DrawText("Average", 550, 195, 25, RED);
-            DrawText("Gantt", 20, 330, 25, RED);
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+    
+    while (!WindowShouldClose()){
+        preview_screen();
+        printf("%d\n",GetFPS());
     }
- 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
- 
-    // TODO: Unload all loaded resources at this point
- 
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
- 
+    CloseWindow();
     return 0;
 }
