ef734b642bb315a7c205e19fdf9c01d66c64fbb2
Saber Mahjoub <saber.mahjoub003@gmail.com>
2023-12-04 19:06:00 +0100

    Version 2 of gantt's synchro

--- src/main2.c
+++ src/main2.c
@@ -2,6 +2,7 @@
 #include "./includes/utils/processes_generator.h"
 #include "./includes/utils/ProcessesTable.h"
 #include "./includes/utils/algo_result.h"
+#include <unistd.h>
 
 #define RAYGUI_IMPLEMENTATION
 #include "../build_raylib/_deps/raygui-src/src/raygui.h"
@@ -18,6 +19,31 @@ Font font;
 
 Algorithm* algorithms = NULL;
 Process* processes = NULL;
+
+int rectangleWidth = 70;
+int rectangleHeight = 50;
+
+
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+    Color color;
+} RectangleData;
+
+RectangleData rects[5];
+
+int nb=5;
+int currentRectangleIndex = 0; // Variable to keep track of current rectangle index
+Color colors[] = { RED, BLUE, BLACK, PINK, YELLOW };
+InstantResultNode tableau[5]= {
+    {0,"P1",{"P1"},1,0,NULL},
+    {1,"P2",{"P2","P1"},2,0,NULL},
+    {2,"P2",{"P1","P1"},2,1,NULL},
+    {3,"P3",{"P1","P3"},1,0,NULL},
+    {4,"P1",{"P1"},1,1,NULL}
+};
 AlgoResult algoResult;
 
 int started=0;
@@ -26,10 +52,42 @@ void render_job_pool(Rectangle boundry){
     DrawRectangleRounded(boundry,borderRadius,20,containerColor);
     DrawTextEx(font,"Job pool",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
 }
-void render_gantt(Rectangle boundry){
+void render_gantt(Rectangle boundry,Rectangle boundry2){
+
     DrawRectangleRounded(boundry,borderRadius,20,containerColor);
     DrawTextEx(font,"Gantt",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+
+
+    if (currentRectangleIndex < nb) {
+            printf("Normally \n");
+            rects[currentRectangleIndex].x = boundry.x + (currentRectangleIndex * rectangleWidth) +5;
+            rects[currentRectangleIndex].y= boundry.y+50;
+            rects[currentRectangleIndex].width= rectangleWidth ;
+            rects[currentRectangleIndex].height= rectangleHeight;
+            rects[currentRectangleIndex].color= colors[currentRectangleIndex];
+            sleep(1);
+            currentRectangleIndex++;
+    }
+    for (int i=0;i<currentRectangleIndex;i++) {
+        DrawRectangle(rects[i].x,rects[i].y,rects[i].width,rects[i].height,rects[i].color);
+        //render_queue(boundry2,i);
+        //DrawRectangle(boundry.x + (i * rectangleWidth) +5,boundry.y+10,rectangleWidth,rectangleHeight,colors[i]);
+    }
+    render_queue(boundry2,currentRectangleIndex);
+
+
 }
+void render_queue(Rectangle boundry,int n){
+    if (n!=nb) {
+        printf("size : %d \n",tableau[n].readyQueueSize);
+         for (int j=0;j<tableau[n].readyQueueSize;j++) {
+            printf(" ready queue size : %d \n",tableau[n].readyQueueSize);
+            DrawRectangle(boundry.x+(j*rectangleWidth)+5, boundry.y+10, rectangleWidth, rectangleHeight, colors[n]);
+        }
+    }
+    else {printf("Not showing the ready queue :\n");}
+}
+
 void render_stats(Rectangle boundry){
     DrawRectangleRounded(boundry,borderRadius,20,containerColor);
     DrawTextEx(font,"Stats",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
@@ -39,8 +97,8 @@ void render_menu(Rectangle boundry){
     DrawTextEx(font,"Menu",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
 }
 void show_gantt_stats(Rectangle ganttBoundry,Rectangle statsBoundry){
-    render_gantt(ganttBoundry);
-    render_stats(statsBoundry);
+    render_gantt(ganttBoundry,statsBoundry);
+    //render_stats(statsBoundry);
     // sleep(1);
 }
 void preview_screen(void){
