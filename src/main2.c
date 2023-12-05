91af7a4f05b1581176c2e5c8a3b6283a47aef8d9
Saber Mahjoub <saber.mahjoub003@gmail.com>
2023-12-06 00:06:29 +0100

    Stats and Gantt layout

--- src/main2.c
+++ src/main2.c
@@ -17,11 +17,20 @@
 #define backgroundColor (Color){255,255,255,1}
 Font font;
 
+
+int nbAlgorithms = 0;
 Algorithm* algorithms = NULL;
 Process* processes = NULL;
 
 int rectangleWidth = 70;
 int rectangleHeight = 50;
+
+int ReadyQrectangleWidth = 70;
+int ReadyQrectangleHeight = 50;
+
+int algoChoice = 0;
+bool editMode=false;
+int value = 0; // Initial value of the spinner
 
 
 typedef struct {
@@ -70,35 +79,96 @@ void render_gantt(Rectangle boundry,Rectangle boundry2){
     }
     for (int i=0;i<currentRectangleIndex;i++) {
         DrawRectangle(rects[i].x,rects[i].y,rects[i].width,rects[i].height,rects[i].color);
-        //render_queue(boundry2,i);
         //DrawRectangle(boundry.x + (i * rectangleWidth) +5,boundry.y+10,rectangleWidth,rectangleHeight,colors[i]);
     }
-    render_queue(boundry2,currentRectangleIndex);
+    render_stats(boundry2,currentRectangleIndex);
 
 
 }
-void render_queue(Rectangle boundry,int n){
+// void render_queue(Rectangle boundry,int n){
+//     if (n!=nb) {
+//         printf("size : %d \n",tableau[n].readyQueueSize);
+//          for (int j=0;j<tableau[n].readyQueueSize;j++) {
+//             printf(" ready queue size : %d \n",tableau[n].readyQueueSize);
+//             DrawRectangle(boundry.x+(j*ReadyQrectangleWidth)+5, boundry.y+5, ReadyQrectangleWidth, ReadyQrectangleHeight, colors[n]);
+//         }
+//     }
+//     else {printf("Not showing the ready queue :\n");}
+// }
+
+void render_stats(Rectangle boundry,int n){
+    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
+    DrawTextEx(font,"Ready Queue",(Vector2){boundry.x+boundry.width/3.3, boundry.y+textPadding},textSize,0,BLACK);
+    //GuiSetStyle(DEFAULT, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
     if (n!=nb) {
         printf("size : %d \n",tableau[n].readyQueueSize);
          for (int j=0;j<tableau[n].readyQueueSize;j++) {
             printf(" ready queue size : %d \n",tableau[n].readyQueueSize);
-            DrawRectangle(boundry.x+(j*rectangleWidth)+5, boundry.y+10, rectangleWidth, rectangleHeight, colors[n]);
+            DrawRectangle(boundry.x+(j*ReadyQrectangleWidth)+5, boundry.y+50, ReadyQrectangleWidth, ReadyQrectangleHeight, colors[n]);
         }
     }
     else {printf("Not showing the ready queue :\n");}
-}
+    DrawTextEx(font,"Current job",(Vector2){boundry.x+boundry.width/8, boundry.y+2.2*ReadyQrectangleHeight},textSize-10,0,BLACK);
+    RectangleData CurrJobRect = {boundry.x+boundry.width/8, boundry.y+2.2*ReadyQrectangleHeight+textSize ,60,40,BLACK};
+    DrawRectangle(CurrJobRect.x,CurrJobRect.y,CurrJobRect.width,CurrJobRect.height,WHITE);
+    DrawRectangleLinesEx((Rectangle){CurrJobRect.x,CurrJobRect.y,CurrJobRect.width,CurrJobRect.height,WHITE},3, BLACK);                          
 
-void render_stats(Rectangle boundry){
-    DrawRectangleRounded(boundry,borderRadius,20,containerColor);
-    DrawTextEx(font,"Stats",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+    //GuiIconText(ICON_REPEAT,"") ;
+    DrawTextEx(font,"Current time",(Vector2){boundry.x+2.2*boundry.width/3, boundry.y+2.2*ReadyQrectangleHeight},textSize-10,0,BLACK);
+    RectangleData CurrTimeRect = {boundry.x + 3*boundry.width/4 , boundry.y+2.2*ReadyQrectangleHeight+textSize ,60,40,WHITE};
+    DrawRectangle(CurrTimeRect.x,CurrTimeRect.y,CurrTimeRect.width,CurrTimeRect.height,WHITE);
+    DrawRectangleLinesEx((Rectangle){CurrTimeRect.x,CurrTimeRect.y,CurrTimeRect.width,CurrTimeRect.height,WHITE},3, BLACK);                          
+
+
+    DrawTextEx(font,"Average rotation time",(Vector2){boundry.x+boundry.width/4.2, boundry.y+3.7*ReadyQrectangleHeight},textSize-10,0,BLACK);
+    RectangleData RotationTimeRect = {boundry.x + boundry.width/3 , boundry.y+4.2*ReadyQrectangleHeight ,100,35,WHITE};
+    DrawRectangle(RotationTimeRect.x,RotationTimeRect.y,RotationTimeRect.width,RotationTimeRect.height,WHITE);
+    DrawRectangleLinesEx((Rectangle){RotationTimeRect.x,RotationTimeRect.y,RotationTimeRect.width,RotationTimeRect.height,WHITE},3, BLACK);                       
+
+    DrawTextEx(font,"Average waiting time",(Vector2){boundry.x+boundry.width/4.2, boundry.y+5*ReadyQrectangleHeight},textSize-10,0,BLACK);
+    RectangleData WaitingTimeRect = {boundry.x + boundry.width/3 , boundry.y+5.5*ReadyQrectangleHeight ,100,35,WHITE};
+    DrawRectangle(WaitingTimeRect.x,WaitingTimeRect.y,WaitingTimeRect.width,WaitingTimeRect.height,WHITE);
+    DrawRectangleLinesEx((Rectangle){WaitingTimeRect.x,WaitingTimeRect.y,WaitingTimeRect.width,WaitingTimeRect.height,WHITE},3, BLACK);                       
+// }
 }
 void render_menu(Rectangle boundry){
     DrawRectangleRounded(boundry,borderRadius,20,containerColor);
     DrawTextEx(font,"Menu",(Vector2){boundry.x+textPadding, boundry.y+textPadding},textSize,0,RED);
+
+    Rectangle DropdownRect = { boundry.x+boundry.x/8, boundry.y+40, boundry.width/2, 35 }; //boundry.y + 40
+    DrawText("Quantum", boundry.x+boundry.x/8, boundry.y+80, 25, BLACK); //+40
+    Rectangle SpinnerRect = { boundry.x+boundry.x/8, 80+DropdownRect.y, boundry.width/2, 35 }; //+80
+    Rectangle Startbutton = { boundry.x+boundry.x/8, SpinnerRect.y+80 , 200, 35}; 
+    Rectangle Resetbutton = { boundry.x+boundry.x/8, Startbutton.y+80, 200, 35};
+
+    const char *dropdownItems =  "Option 1;Option 2;Option3";
+        //if (editMode) GuiLock();
+
+        DrawRectangle(5, 5, 392.5, 175, (Color){ 220, 220, 220, 255 });
+        GuiSetStyle(BUTTON, TEXT_SIZE, 40);
+        GuiSetStyle(BUTTON,TEXT_COLOR_NORMAL,ColorToInt(BLACK));
+        GuiSetStyle(BUTTON,BORDER_COLOR_NORMAL,ColorToInt(BLACK));
+        GuiSetStyle(BUTTON, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
+        GuiUnlock();
+        GuiSetStyle(BUTTON,BASE_COLOR_NORMAL,ColorToInt(WHITE));
+        GuiSetStyle(BUTTON,TEXT_PADDING,1);
+        //GuiSetStyle(BUTTON, BORDER_COLOR_NORMAL, borderColorInt);
+        GuiSetStyle(DROPDOWNBOX,TEXT_COLOR_NORMAL,ColorToInt(BLACK));
+        GuiSetStyle(DROPDOWNBOX, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
+        GuiSetStyle(DROPDOWNBOX,BORDER_COLOR_NORMAL,ColorToInt(BLACK));
+        GuiSetStyle(DROPDOWNBOX,BASE_COLOR_NORMAL,ColorToInt(WHITE));
+        GuiSetStyle(DROPDOWNBOX, TEXT_PADDING, 0);
+        if (GuiDropdownBox(DropdownRect, dropdownItems, &algoChoice, editMode)) editMode=!editMode;
+        if(GuiButton(Startbutton, "Start")){
+            
+        }
+        if (GuiButton(Resetbutton, GuiIconText(ICON_REPEAT,"Reset"))){
+
+        }
+        GuiSpinner(SpinnerRect, NULL, &value, -10, 10, false);
 }
 void show_gantt_stats(Rectangle ganttBoundry,Rectangle statsBoundry){
     render_gantt(ganttBoundry,statsBoundry);
-    //render_stats(statsBoundry);
     // sleep(1);
 }
 void preview_screen(void){
@@ -144,7 +214,7 @@ int main(void){
     font = LoadFont("../assets/fonts/DelaGothicOne-Regular.ttf");
  
     SetTargetFPS(60);
-    
+
     while (!WindowShouldClose()){
         preview_screen();
         printf("%d\n",GetFPS());
